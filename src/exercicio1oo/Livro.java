4fb857ad923ce4dd689282e1ca53a0fc6e8bf3d1
Lucas de Oliveira <lucas.oliveira1512@edu.udesc.br>
2025-05-04 09:52:38 -0300

    Livro

---
+++ src/exercicio1oo/Livro.java
@@ -0,0 +1,10 @@
+package exercicio1oo;
+
+public class Livro      {
+
+    String titulo;
+    String autor;
+    String genero;
+    String emprestado;
+
+}
---
+++ src/exercicio1oo/TesteLivro.java
@@ -0,0 +1,19 @@
+package exercicio1oo;
+
+public class TesteLivro {
+
+    public static void main(String[] args) {
+
+        Livro livro = new Livro();
+        livro.titulo = "Harry Potter";
+        livro.autor = "J.K. Rowling";
+        livro.genero = "Fantasia";
+        livro.emprestado = "Sim";
+
+        System.out.println("Título: " + livro.titulo);
+        System.out.println("Autor: " + livro.autor);
+        System.out.println("Gênero: " + livro.genero);
+        System.out.println("Emprestado: " + livro.emprestado);
+
+    }
+}
