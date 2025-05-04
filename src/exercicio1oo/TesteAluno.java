98b9dc36f2826bccd60f642329cc8866aa7392e3
Lucas de Oliveira <lucas.oliveira1512@edu.udesc.br>
2025-05-04 09:22:11 -0300

    Commit Classe Aluno

---
+++ src/exercicio1oo/TesteAluno.java
@@ -0,0 +1,19 @@
+package exercicio1oo;
+
+public class TesteAluno {
+    public static void main(String[] args) {
+        Aluno aluno = new Aluno();
+        aluno.matricula = "12345";
+        aluno.nome = "Lucas";
+        aluno.idade = 23;
+        aluno.nota1 = 7;
+        aluno.nota2 = 8;
+        aluno.nota3 = 6;
+        aluno.nota4 = 7;
+
+        System.out.println("Matrícula: " + aluno.matricula);
+        System.out.println("Nome: " + aluno.nome);
+        System.out.println("Idade: " + aluno.idade);
+        System.out.println("Notas: " + aluno.nota1 + ", " + aluno.nota2 + ", " + aluno.nota3 + ", " + aluno.nota4);
+    }
+}
