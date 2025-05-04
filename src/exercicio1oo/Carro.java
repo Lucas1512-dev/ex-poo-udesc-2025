012de072908e49104f2febc97189743356e0b374
Lucas de Oliveira <lucas.oliveira1512@edu.udesc.br>
2025-05-04 09:46:56 -0300

    Commit Classe Carro

---
+++ src/exercicio1oo/Carro.java
@@ -0,0 +1,10 @@
+package exercicio1oo;
+
+public class Carro {
+
+    String modelo;
+    String marca;
+    int ano;
+    double velocidade;
+
+}
---
+++ src/exercicio1oo/TesteCarro.java
@@ -0,0 +1,19 @@
+package exercicio1oo;
+
+public class TesteCarro {
+
+    public static void main(String[] args) {
+
+        Carro carro = new Carro();
+        carro.modelo = "Gol";
+        carro.marca = "Volkswagen";
+        carro.ano = 1995;
+        carro.velocidade = 160;
+
+        System.out.println("Modelo: " + carro.modelo);
+        System.out.println("Marca: " + carro.marca);
+        System.out.println("Ano: " + carro.ano);
+        System.out.println("Velocidade: " + carro.velocidade);
+
+    }
+}
