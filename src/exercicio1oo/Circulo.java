6f709a064d7cb42c4286b6196020ecef7ab1816d
Lucas de Oliveira <lucas.oliveira1512@edu.udesc.br>
2025-05-04 09:34:12 -0300

    Commit Classe Circulo

---
+++ src/exercicio1oo/Circulo.java
@@ -0,0 +1,7 @@
+package exercicio1oo;
+
+public class Circulo {
+
+    double raio;
+
+}
---
+++ src/exercicio1oo/TesteCirculo.java
@@ -0,0 +1,12 @@
+package exercicio1oo;
+
+public class TesteCirculo {
+
+    public static void main(String[] args) {
+
+        Circulo circulo = new Circulo();
+        circulo.raio = 7.5;
+
+        System.out.println("Raio: " + circulo.raio);
+    }
+}
