eb724323733f7b24ccbdb16894c66522590c8da1
Lucas de Oliveira <lucas.oliveira1512@edu.udesc.br>
2025-05-11 16:58:32 -0300

    Commit Classe Retangulo/TestaRetangulo

---
+++ src/exercicio1oo/Retangulo.java
@@ -0,0 +1,7 @@
+package exercicio1oo;
+
+    class Retangulo {
+        double largura;
+        double altura;
+
+}
---
+++ src/exercicio1oo/TestaRetangulo.java
@@ -0,0 +1,14 @@
+package exercicio1oo;
+
+    class TestaRetangulo {
+        public static void main(String[] args) {
+            Retangulo r = new Retangulo();
+            r.largura = 5;
+            r.altura = 3;
+
+            System.out.println("Largura: " + r.largura);
+            System.out.println("Altura: " + r.altura);
+
+        }
+
+}
